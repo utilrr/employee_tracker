547c4592325833365b4a1531bed136109ab64b1b
utilrr <gmarkghealy@gmail.com>
2022-05-03 22:25:24 -0400

    reconfigured async await

--- index.js
+++ index.js
@@ -2,29 +2,104 @@ const inquirer = require("inquirer");
 const db = require("./db/connection");
 const cTable = require("console.table");
 
-const fs = require("fs");
+const { viewDepts, addDept, deleteDept } = require("./controllers/dept");
+const { viewRoles, addRole, deleteRole } = require("./controllers/role");
+const {
+  viewEmployees,
+  addEmployee,
+  updateEmployeeRole,
+  deleteEmployee,
+} = require("./controllers/emp");
 
 // TODO: Create an array of questions for user input
-const questions = [
-  {
-    type: "list",
-    name: "options",
-    message: "What task would you like to accomplish?",
-    choices: [
-      "View all departments",
-      "View all roles",
-      "View all employees",
-      "Add a department",
-      "Add a role",
-      "Add an employees",
-      "Update employee role",
-    ],
-    when: ({ confirmOptions }) => {
-      if (confirmOptions) {
-        return true;
-      } else {
-        return false;
-      }
+const init = async () => {
+  const { choice } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "options",
+      message: "What task would you like to accomplish?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employees",
+        "Update employee role",
+        "Delete a department",
+        "Delete a role",
+        "Delete an employee",
+        "Done",
+      ],
     },
-  },
-];
+  ]);
+
+  // switch for each selected choice
+  switch (choice) {
+    case "view all departments":
+      let [deptRows] = await viewDepts();
+      console.table(deptRows);
+      init();
+      break;
+
+    case "view all roles":
+      let [roleRows] = await viewRoles();
+      console.table(roleRows);
+      init();
+      break;
+
+    case "view all employees":
+      let [empRows] = await viewEmployees();
+      console.table(empRows);
+      init();
+      break;
+
+    case "add a department":
+      await addDept();
+      console.log("Department added successfully.");
+      init();
+      break;
+
+    case "add a role":
+      await addRole();
+      console.log("Role added successfully.");
+      init();
+      break;
+
+    case "add an employee":
+      await addEmployee();
+      console.log("Employee added successfully.");
+      init();
+      break;
+
+    case "update employee role":
+      await updateEmployeeRole();
+      console.log("The employees role has been updated.");
+      init();
+      break;
+
+    case "delete a department":
+      await deleteDept();
+      console.log("The department has been deleted.");
+      init();
+      break;
+
+    case "delete a role":
+      await deleteRole();
+      console.log("The role has been deleted.");
+      init();
+      break;
+
+    case "delete an employee":
+      await deleteEmployee();
+      console.log("The employee has been deleted.");
+      init();
+      break;
+
+    case "done":
+      console.log("Done");
+      break;
+  }
+};
+
+init();
